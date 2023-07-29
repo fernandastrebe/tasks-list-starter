8b2e6831316281c9c17473e74329c17a7fe75cfb
Fernanda Strebe <feh_strebe@hotmail.com>
2023-07-28 21:56:13 -0300

    feat: iniciado projeto de listas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <InputList/>
+    <ListsBody/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/HeaderPage.vue'
+import InputList from './components/InputList.vue'
+import ListsBody from './components/ListsBody.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    InputList,
+    ListsBody
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+     <h1 class="to">to</h1>
+     <h1 class="do">do</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  background-color: #B0E0E6;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.to {
+  color: #fff;
+  font-weight: bold;
+  font-size: 60px;
+}
+
+.do {
+  color: #1E6F9F;
+  font-weight: bold;
+  font-size: 60px;
+}
+
+</style>
