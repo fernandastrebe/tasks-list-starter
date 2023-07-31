9561bdcae70d4f2e18c72e12b31fb5481fc46476
Fernanda Strebe <feh_strebe@hotmail.com>
2023-07-31 19:30:47 -0300

    feat: ajustado o arquivo do eslint, porque estava dando erro no import dos componentes no teste

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,6 @@ parserOptions: {
     requireConfigFile: false; // <== ADD THIS
     ecmaVersion: 2020; // Allows for the parsing of modern ECMAScript features
     sourceType: 'module'; // Allows for the use of imports
+    "extends": ["@salesforce/eslint-config-lwc/recommended"]
+    
 }
